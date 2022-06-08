709fb6abdc13cf6fc289bc3bdbddb8ebbd2b02d0
Alexis Donato <alek6.don@gmail.com>
2022-06-08 16:11:34 +0200

    Symfony Record3 slight mods

--- Exos/Symfony/Record3/src/Controller/RegistrationController.php
+++ Exos/Symfony/Record3/src/Controller/RegistrationController.php
@@ -47,7 +47,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('admin@admin.com', 'Meta Master K6'))
+                    ->from(new Address('admin@disc&artist.com', 'K6 Mail Bot'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
